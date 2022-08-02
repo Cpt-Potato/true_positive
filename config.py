c769178de6fbe59a54ed6e01c2474e1b087e4f2e
Alexander Morozov <reverse43@gmail.com>
2022-08-02 21:31:45 +0300

    Add logger config

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 from dotenv import load_dotenv
@@ -7,3 +8,9 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 TOKEN = os.getenv("TOKEN")
 ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD")
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(levelname)s - %(asctime)s - %(name)s - %(message)s"
+)
