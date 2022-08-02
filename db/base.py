7b761dad6000a1f285b6ee4c1c56b42068838f3e
Alexander Morozov <reverse43@gmail.com>
2022-08-02 21:31:26 +0300

    Use "with"

--- db/base.py
+++ db/base.py
@@ -7,25 +7,18 @@ from config import DATABASE_URL
 
 Base = declarative_base()
 
+engine = create_async_engine(DATABASE_URL)
+
 
 async def create_table():
-    engine = create_async_engine(DATABASE_URL)
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-    await engine.dispose()
 
 
-async def get_session():
-    engine = create_async_engine(DATABASE_URL)
-    # expire_on_commit=False will prevent attributes from being expired
-    # after commit.
+async def get_session() -> AsyncSession:
     async_session = sessionmaker(
         engine, expire_on_commit=False, class_=AsyncSession
     )
     async with async_session() as session:
-        async with session.begin():
-            yield session
-    await engine.dispose()
-
-
-asyncio.run(create_table())
+        # async with session.begin():
+        yield session
