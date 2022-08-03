ee4454fe1969dd9666702704eebbdc2fd9b8d6d1
Alexander Morozov <reverse43@gmail.com>
2022-08-03 18:57:12 +0300

    Fix session getter

--- db/base.py
+++ db/base.py
@@ -1,24 +1,20 @@
-import asyncio
-
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 from config import DATABASE_URL
 
 Base = declarative_base()
-
-engine = create_async_engine(DATABASE_URL)
+engine = create_async_engine(DATABASE_URL, echo=True)
 
 
 async def create_table():
+    """Creates db table if it doesn't already exist"""
     async with engine.begin() as conn:
+        # await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
 
 
 async def get_session() -> AsyncSession:
-    async_session = sessionmaker(
-        engine, expire_on_commit=False, class_=AsyncSession
-    )
+    async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
     async with async_session() as session:
-        # async with session.begin():
-        yield session
+        return session
