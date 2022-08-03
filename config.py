58cc2e5a905aafd4876ba5302e117cb868680fc6
Alexander Morozov <reverse43@gmail.com>
2022-08-03 22:44:57 +0300

    Move bot and dp to config

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import logging
 import os
 
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,8 +11,10 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 TOKEN = os.getenv("TOKEN")
 ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD")
 
+bot = Bot(token=TOKEN)  # , parse_mode=types.ParseMode.HTML)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
 logger = logging.getLogger(__name__)
 logging.basicConfig(
-    level=logging.INFO,
-    format="%(levelname)s - %(asctime)s - %(name)s - %(message)s"
+    level=logging.INFO, format="%(levelname)s - %(asctime)s - %(name)s - %(message)s"
 )
